994a081a3dacdab787e397cdd6b27041c40ff547
Lucas Magalhães <107592724+Lucas-magalhaes1@users.noreply.github.com>
2023-08-03 16:02:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,23 @@ function blockIP(ip) {
     alert(`Seu IP ${ip} foi bloqueado temporariamente por exceder o limite de solicitações.`);
   }
 }
+
+const btnElement = document.querySelector('button');
+const allowedRequests = 10; // Limite de solicitações permitidas em um curto período de tempo
+let requestCount = 0;
+
+function handleButtonClick() {
+  requestCount++;
+  if (requestCount > allowedRequests) {
+    btnElement.disabled = true;
+    setTimeout(() => {
+      requestCount = 0;
+      btnElement.disabled = false;
+    }, 5000); // Bloquear temporariamente o botão por 5 segundos
+    alert(`Você atingiu o limite de solicitações permitidas. Tente novamente em alguns segundos.`);
+  } else {
+    blockIP('192.168.0.1'); // Substitua '192.168.0.1' pelo IP do cliente real
+  }
+}
+
+btnElement.addEventListener('click', handleButtonClick);
