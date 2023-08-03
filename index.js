8cb7a353fc958fb9cc598c6c25d23b5a88f64416
Lucas Magalhães <107592724+Lucas-magalhaes1@users.noreply.github.com>
2023-08-03 16:07:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ function blockIP(ip) {
   rateLimit[ip] = (rateLimit[ip] || 0) + 1;
   if (rateLimit[ip] > 10) {
     alert(`Seu IP ${ip} foi bloqueado temporariamente por exceder o limite de solicitações.`);
+    btnElement.disabled = true; // Bloquear o botão
+    setTimeout(() => {
+      rateLimit[ip] = 0; // Zerar o contador de solicitações
+      btnElement.disabled = false; // Desbloquear o botão após o tempo determinado
+    }, 5000); // Bloquear temporariamente o botão por 5 segundos
   }
 }
 
@@ -16,15 +21,29 @@ let requestCount = 0;
 function handleButtonClick() {
   requestCount++;
   if (requestCount > allowedRequests) {
-    btnElement.disabled = true;
-    setTimeout(() => {
-      requestCount = 0;
-      btnElement.disabled = false;
-    }, 5000); // Bloquear temporariamente o botão por 5 segundos
-    alert(`Você atingiu o limite de solicitações permitidas. Tente novamente em alguns segundos.`);
-  } else {
     blockIP('192.168.0.1'); // Substitua '192.168.0.1' pelo IP do cliente real
+    requestCount = 0; // Zerar o contador de solicitações
+    return;
   }
+  
+  setTimeout(() => {
+    requestCount = 0; // Zerar o contador de solicitações após o tempo determinado
+  }, 5000); // Tempo de reset das solicitações (5 segundos)
 }
 
 btnElement.addEventListener('click', handleButtonClick);
+
+// Função para validar o formulário antes de ser enviado
+function validateForm() {
+  // Verificar se o reCAPTCHA foi concluído
+  const response = grecaptcha.getResponse();
+  if (response.length === 0) {
+    alert('Por favor, complete o reCAPTCHA antes de enviar o formulário.');
+    return false; // Impede o envio do formulário
+  }
+
+  // Aqui você pode adicionar outras verificações e validações do formulário, se necessário.
+
+  return true; // Permite o envio do formulário
+}
+
